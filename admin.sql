990af5f77aebbd24fe7be503d25e61d22b726df8
venom95 <li_tz95@163.com>
2018-11-27 01:08:41 +0800

    默认数据库 2.0

--- admin.sql
+++ admin.sql
@@ -10,43 +10,10 @@ Target Server Type    : MYSQL
 Target Server Version : 50553
 File Encoding         : 65001
 
-Date: 2018-11-05 11:49:04
+Date: 2018-11-27 01:07:39
 */
 
 SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for account
--- ----------------------------
-DROP TABLE IF EXISTS `account`;
-CREATE TABLE `account` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `account` varchar(32) NOT NULL,
-  `onclick_num` int(11) NOT NULL DEFAULT '0',
-  `create_time` varchar(32) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Table structure for minisoft
--- ----------------------------
-DROP TABLE IF EXISTS `minisoft`;
-CREATE TABLE `minisoft` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `on_off` tinyint(1) NOT NULL DEFAULT '0',
-  `create_time` varchar(19) NOT NULL,
-  `code` varchar(32) NOT NULL,
-  `mark` varchar(10) NOT NULL DEFAULT '',
-  `email` varchar(32) NOT NULL DEFAULT '',
-  `monitor` varchar(32) NOT NULL DEFAULT '',
-  `onclick_num` int(11) NOT NULL DEFAULT '0',
-  `user_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_minisoft_user2_idx` (`user_id`) USING BTREE,
-  CONSTRAINT `minisoft_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=748 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for permission
@@ -60,7 +27,15 @@ CREATE TABLE `permission` (
   `level` int(11) NOT NULL DEFAULT '0',
   `icon` varchar(32) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of permission
+-- ----------------------------
+INSERT INTO `permission` VALUES ('1', '后台管理', '', '0', '1', 'home');
+INSERT INTO `permission` VALUES ('2', '权限管理', '/admin/permission/index', '1', '2', '');
+INSERT INTO `permission` VALUES ('3', '用户管理', '/admin/user/index', '1', '2', '');
+INSERT INTO `permission` VALUES ('4', '角色管理', '/admin/role/index', '1', '2', '');
 
 -- ----------------------------
 -- Table structure for role
@@ -71,7 +46,12 @@ CREATE TABLE `role` (
   `name` varchar(32) NOT NULL,
   `description` varchar(256) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES ('1', '超级管理员', 'ROOT');
 
 -- ----------------------------
 -- Table structure for role_permission
@@ -86,6 +66,10 @@ CREATE TABLE `role_permission` (
   CONSTRAINT `role_permission_ibfk_1` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `role_permission_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of role_permission
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for user
@@ -104,4 +88,9 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   KEY `fk_user_role_idx` (`role_id`) USING BTREE,
   CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'admin', 'e10adc3949ba59abbe56e057f20f883e', '', 'Administrator', '1', '1', '', '1');
